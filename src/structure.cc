b966cb85d913a78d74794b0b5467cf9aa614e675
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-07-30 02:09:19 +0700

    update texture cube

--- src/structure.cc
+++ src/structure.cc
@@ -137,55 +137,55 @@ Box(Coord3D pos, Coord3D size, CubeTexture * cubeTexture)
     // Depan
     glBindTexture(GL_TEXTURE_2D, cubeTexture->front.GetTexture());
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, 0, 0));
-        glVertex3f(up3add(pos, sx, 0, 0));
-        glVertex3f(up3add(pos, sx, sy, 0));
-        glVertex3f(up3add(pos, 0, sy, 0));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, 0, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, 0, 0));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, sy, 0));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, sy, 0));
     glEnd();
 
     // Kiri
     glBindTexture(GL_TEXTURE_2D, cubeTexture->left.GetTexture());
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, 0, 0));
-        glVertex3f(up3add(pos, 0, 0, sz));
-        glVertex3f(up3add(pos, 0, sy, sz));
-        glVertex3f(up3add(pos, 0, sy, 0));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, 0, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, 0, 0, sz));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, 0, sy, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, sy, 0));
     glEnd();
 
     // Atas
     glBindTexture(GL_TEXTURE_2D, cubeTexture->top.GetTexture());
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, sy, 0));
-        glVertex3f(up3add(pos, sx, sy, 0));
-        glVertex3f(up3add(pos, sx, sy, sz));
-        glVertex3f(up3add(pos, 0, sy, sz));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, sy, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, sy, 0));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, sy, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, sy, sz));
     glEnd();
 
     // Kanan
     glBindTexture(GL_TEXTURE_2D, cubeTexture->right.GetTexture());
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, sx, sy, 0));
-        glVertex3f(up3add(pos, sx, sy, sz));
-        glVertex3f(up3add(pos, sx, 0, sz));
-        glVertex3f(up3add(pos, sx, 0, 0));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, sx, sy, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, sy, sz));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, 0, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, sx, 0, 0));
     glEnd();
 
     // Bawah
     glBindTexture(GL_TEXTURE_2D, cubeTexture->bottom.GetTexture());
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, sx, 0, 0));
-        glVertex3f(up3add(pos, 0, 0, 0));
-        glVertex3f(up3add(pos, 0, 0, sz));
-        glVertex3f(up3add(pos, sx, 0, sz));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, sx, 0, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, 0, 0, 0));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, 0, 0, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, sx, 0, sz));
     glEnd();
 
     // Belakang
     glBindTexture(GL_TEXTURE_2D, cubeTexture->back.GetTexture());
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, sy, sz));
-        glVertex3f(up3add(pos, sx, sy, sz));
-        glVertex3f(up3add(pos, sx, 0, sz));
-        glVertex3f(up3add(pos, 0, 0, sz));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, sy, sz));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, sy, sz));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, 0, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, 0, sz));
     glEnd();
 }
 
@@ -200,59 +200,59 @@ Box(Coord3D pos, Coord3D size, CubeTexture * cubeTexture, CubeFaceColor * cubeFa
     glBindTexture(GL_TEXTURE_2D, cubeTexture->front.GetTexture());
     glColor3f(up3(cubeFaceColor->front));
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, 0, 0));
-        glVertex3f(up3add(pos, sx, 0, 0));
-        glVertex3f(up3add(pos, sx, sy, 0));
-        glVertex3f(up3add(pos, 0, sy, 0));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, 0, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, 0, 0));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, sy, 0));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, sy, 0));
     glEnd();
 
     // Kiri
     glBindTexture(GL_TEXTURE_2D, cubeTexture->left.GetTexture());
     glColor3f(up3(cubeFaceColor->left));
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, 0, 0));
-        glVertex3f(up3add(pos, 0, 0, sz));
-        glVertex3f(up3add(pos, 0, sy, sz));
-        glVertex3f(up3add(pos, 0, sy, 0));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, 0, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, 0, 0, sz));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, 0, sy, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, sy, 0));
     glEnd();
 
     // Atas
     glBindTexture(GL_TEXTURE_2D, cubeTexture->top.GetTexture());
     glColor3f(up3(cubeFaceColor->top));
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, sy, 0));
-        glVertex3f(up3add(pos, sx, sy, 0));
-        glVertex3f(up3add(pos, sx, sy, sz));
-        glVertex3f(up3add(pos, 0, sy, sz));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, sy, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, sy, 0));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, sy, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, sy, sz));
     glEnd();
 
     // Kanan
     glBindTexture(GL_TEXTURE_2D, cubeTexture->right.GetTexture());
     glColor3f(up3(cubeFaceColor->right));
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, sx, sy, 0));
-        glVertex3f(up3add(pos, sx, sy, sz));
-        glVertex3f(up3add(pos, sx, 0, sz));
-        glVertex3f(up3add(pos, sx, 0, 0));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, sx, sy, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, sy, sz));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, 0, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, sx, 0, 0));
     glEnd();
 
     // Bawah
     glBindTexture(GL_TEXTURE_2D, cubeTexture->bottom.GetTexture());
     glColor3f(up3(cubeFaceColor->bottom));
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, sx, 0, 0));
-        glVertex3f(up3add(pos, 0, 0, 0));
-        glVertex3f(up3add(pos, 0, 0, sz));
-        glVertex3f(up3add(pos, sx, 0, sz));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, sx, 0, 0));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, 0, 0, 0));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, 0, 0, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, sx, 0, sz));
     glEnd();
 
     // Belakang
     glBindTexture(GL_TEXTURE_2D, cubeTexture->back.GetTexture());
     glColor3f(up3(cubeFaceColor->back));
     glBegin(RENDER_MODE);
-        glVertex3f(up3add(pos, 0, sy, sz));
-        glVertex3f(up3add(pos, sx, sy, sz));
-        glVertex3f(up3add(pos, sx, 0, sz));
-        glVertex3f(up3add(pos, 0, 0, sz));
+        glTexCoord2f(1.0, 0.0); glVertex3f(up3add(pos, 0, sy, sz));
+        glTexCoord2f(1.0, 1.0); glVertex3f(up3add(pos, sx, sy, sz));
+        glTexCoord2f(0.0, 1.0); glVertex3f(up3add(pos, sx, 0, sz));
+        glTexCoord2f(0.0, 0.0); glVertex3f(up3add(pos, 0, 0, sz));
     glEnd();
 }
